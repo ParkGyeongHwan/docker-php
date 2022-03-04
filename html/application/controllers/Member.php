940ee49f074da1cc893baa5133e8befb024cb9bc
parkgyeonghwan <ghksdlajwu@gmail.com>
2022-03-04 09:13:05 +0900

    codeigniter로 게시판 만들기

---
+++ html/application/controllers/Member.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Member extends CI_Controller {
+
+	public function index() {
+		echo "회원 프로그램";
+	}
+
+
+    public function input() {
+        echo "회원 가입";
+    }
+
+    public function login() {
+        echo "로그인";
+    }
+
+    public function update() {
+        echo "회원정보수정";
+    }
+}
---
+++ html/application/views/board/update.php
@@ -0,0 +1,7 @@
+
+
+<form>
+    <input type="text" name="title"> <br />
+    <textarea name="content"></textarea> <br />
+    <input type="submit" value="글등록">
+</form>
